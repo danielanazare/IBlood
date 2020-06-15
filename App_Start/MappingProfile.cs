4fa6550b5893c2ac1cd7e7e7188acccd9631a4a6
danielanazare <nazare_daniela@yahoo.com>
2020-06-15 18:10:29 +0300

    RESTfulWebAPIsForDonor

---
+++ App_Start/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using IBlood002.Dtos;
+using IBlood002.Models;
+
+namespace IBlood002.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        
+        public MappingProfile()
+        {
+            //uses reflexion to map based on their property names
+            Mapper.CreateMap<Donor, DonorDto>();
+            Mapper.CreateMap<DonorDto, Donor>();
+        }
+    }
+}
---
+++ Dtos/DonorDto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using IBlood002.Models;
+
+namespace IBlood002.Dtos
+{
+    public class DonorDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string LastName { get; set; }
+
+
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        [StringLength(13)]
+        public string CNP { get; set; }
+
+
+        [StringLength(10)]
+        public string Phone { get; set; }
+
+        [Display(Name = "Date of Birth")]
+       // [Min18Years]
+        public DateTime BirthDate { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public char Sex { get; set; }
+
+       
+        public bool IsSubscribedToNewsletter { get; set; }
+
+
+
+        //entity-framework treats this property as a foreign key
+        [Required]
+        
+        public byte BloodTypeId { get; set; }
+
+    }
+}
